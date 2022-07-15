163df49fd6f7a07568962811e169f8ef97dc73f6
sumit <dev123@gmail.com>
2022-07-15 21:44:53 +0530

    logs removed

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ function App() {
   const fetchFileById = async (id, isDirectory) => {
     const response = await fetch(`${base_url}/files/${id}`);
     const data = await response.json();
-    console.log(data);
     setContent(data);
     setIsDirectory(isDirectory);
   };
+  
   const UpdateFileByID = async (id) => {
     const response = await fetch(`${base_url}/files/${id}`, {
       method: "PUT",
@@ -45,11 +45,11 @@ function App() {
       body: JSON.stringify(content),
     });
   };
+
   const handleOnClick = (id, isDirectory) => {
     const contentData = fileData.find((data) => data.id === id);
     setContent(contentData);
     setIsDirectory(isDirectory);
-    console.log("contentData", contentData);
   };
 
   function handleEditorChange(value, event) {
@@ -62,28 +62,25 @@ function App() {
   const onNameClick = ({ defaultOnClick, nodeData }) => {
     setEdit(false);
     defaultOnClick();
-    console.log("nodeData", nodeData);
+    
     handleOnClick(nodeData.id, nodeData.isDirectory);
     // fetchFileById(nodeData.id,nodeData.isDirectory)
   };
 
   const handleSave = () => {
-    console.log("content state ", content);
+    
     const foundIndex = fileData.findIndex((t) => t.id === content.id);
-    console.log("files array index values", foundIndex);
     if (foundIndex !== -1) {
       let temp = fileData;
       temp[foundIndex].content = content.content;
-      console.log("updated data file", temp);
       setFileData(temp);
     }
     setEdit(false);
   };
-  console.log("Tree data", treeData);
+  
   return (
     <>
       <Header />
-
       <div className="container">
         <div className="left">
           <FolderTree
@@ -112,7 +109,6 @@ function App() {
             value={content && content?.content}
             onChange={handleEditorChange}
             theme="vs-dark"
-            onMount={() => console.log("mount")}
           />
         </div>
       </div>
